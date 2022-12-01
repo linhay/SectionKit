e2f9182a62f4e690c9942702bbcbcfb36e7bd3f3
linhey <is.linhey@outlook.com>
2022-12-01 16:58:13 +0800

    chore: supplementary(kind: SKSupplementaryKind, at row: Int)

--- Sources/SectionKit/CollectionBaseProtocol/SKCSectionProtocol.swift
+++ Sources/SectionKit/CollectionBaseProtocol/SKCSectionProtocol.swift
@@ -5,7 +5,22 @@
 //  Created by linhey on 2022/8/12.
 //
 
-import Foundation
+import UIKit
  
 public typealias SKCBaseSectionProtocol = SKCSectionActionProtocol & SKCDataSourceProtocol & SKCDelegateProtocol
 public typealias SKCSectionProtocol = SKCBaseSectionProtocol & SKCViewDelegateFlowLayoutProtocol & SKCSectionActionProtocol
+
+public extension SKCDataSourceProtocol where Self: SKCViewDelegateFlowLayoutProtocol {
+    
+    func supplementary(kind: SKSupplementaryKind, at row: Int) -> UICollectionReusableView? {
+        switch kind {
+        case .header:
+            return headerView
+        case .footer:
+            return footerView
+        case .cell, .custom:
+            return nil
+        }
+    }
+    
+}

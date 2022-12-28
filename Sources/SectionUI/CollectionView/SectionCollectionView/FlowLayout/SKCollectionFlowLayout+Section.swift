2a1ce58275091d96a957bf23c6bb36f97bedd160
linhey <is.linhey@outlook.com>
2022-12-28 17:33:55 +0800

    chore: SKCollectionFlowLayout.Decoration API

--- Sources/SectionUI/CollectionView/SectionCollectionView/FlowLayout/SKCollectionFlowLayout+Section.swift
+++ Sources/SectionUI/CollectionView/SectionCollectionView/FlowLayout/SKCollectionFlowLayout+Section.swift
@@ -20,3 +20,19 @@ public extension SKCollectionFlowLayout.BindingKey where Value == Int {
     }
     
 }
+
+public extension SKCollectionFlowLayout.Decoration {
+ 
+    public init(_ section: SKCSectionActionProtocol,
+                viewType: SKCollectionFlowLayout.DecorationView.Type,
+                zIndex: Int = -1,
+                layout: [SKCollectionFlowLayout.DecorationLayout] = [.header, .cells, .footer],
+                insets: UIEdgeInsets = .zero) {
+        self.init(sectionIndex: .init(section),
+                  viewType: viewType,
+                  zIndex: zIndex,
+                  layout: layout,
+                  insets: insets)
+    }
+    
+}

9e4ec7b7876b99a1fc161d5e282cb5e56b5252e1
linhey <is.linhey@outlook.com>
2023-03-09 15:52:36 +0800

    chore: 新增注释

--- Sources/SectionUI/Selection/SKBinding.swift
+++ Sources/SectionUI/Selection/SKBinding.swift
@@ -6,32 +6,48 @@
 //
 
 import Foundation
-import Combine
 
 @propertyWrapper
 public struct SKBinding<Value> {
-
-    public var publisher: AnyPublisher<Value, Never> {
-        subject.eraseToAnyPublisher()
-    }
-    private var subject = PassthroughSubject<Value, Never>()
     
+    // 包装的值
     public var wrappedValue: Value {
         get { _get() }
         nonmutating set { _set(newValue) }
     }
-
+    
+    // 返回 SKBinding 的实例
     public var projectedValue: SKBinding<Value> { self }
     
     private let _get: () -> Value
-     private let _set: (Value) -> Void
-
+    private let _set: (Value) -> Void
+    
+    // 初始化方法，接受一个 getter 和 setter
     public init(get: @escaping () -> Value, set: @escaping (Value) -> Void) {
         self._get = get
         self._set = set
     }
     
-    public init<Root>(on object: Root, keyPath: ReferenceWritableKeyPath<Root, Value>)  {
+}
+
+public extension SKBinding {
+    
+    // 创建一个常量绑定
+    static func constant(_ value: Value) -> SKBinding<Value> {
+        .init(get: { value }, set: { _ in })
+    }
+    
+}
+
+public extension SKBinding {
+    
+    // 从一个现有的绑定创建一个新的绑定
+    init(_ source: SKBinding<Value>) {
+        self.init(get: source._get, set: source._set)
+    }
+    
+    // 从一个对象的 keyPath 创建一个绑定
+    init<Root>(on object: Root, keyPath: ReferenceWritableKeyPath<Root, Value>)  {
         self.init {
             object[keyPath: keyPath]
         } set: { value in
@@ -39,20 +55,13 @@ public struct SKBinding<Value> {
         }
     }
     
-    public init<Root>(on object: Root, keyPath: ReferenceWritableKeyPath<Root, Value>, default: Value) where Root: AnyObject {
+    // 从一个对象的 keyPath 和默认值创建一个绑定
+    init<Root>(on object: Root, keyPath: ReferenceWritableKeyPath<Root, Value>, default: Value) where Root: AnyObject {
         self.init { [weak object] in
             object?[keyPath: keyPath] ?? `default`
         } set: { [weak object] value in
             object?[keyPath: keyPath] = value
         }
-    }
-
-    public init(_ source: SKBinding<Value>) {
-        self.init(get: source._get, set: source._set)
-    }
-
-    public static func constant(_ value: Value) -> SKBinding<Value> {
-        .init(get: { value }, set: { _ in })
     }
     
 }

903c6826dd371ce1dbdc92e7ecb65724cfeac8e9
linhey <is.linhey@outlook.com>
2023-08-31 18:25:42 +0800

    chore: SKSelectionIdentifiableSequence 重构

--- Sources/SectionUI/Selection/SKSelectionIdentifiableSequence.swift
+++ Sources/SectionUI/Selection/SKSelectionIdentifiableSequence.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by linhey on 2022/9/9.
 //
@@ -13,22 +13,8 @@ public class SKSelectionIdentifiableSequence<Element: SKSelectionProtocol, ID: H
     /// 是否保证选中在当前序列中是否唯一 | default: true
     public var isUnique: Bool
     
-    public private(set) lazy var itemChangedPublisher: AnyPublisher<SKSelectionIdentifiableSequence, Never> = {
-        Deferred { [weak self] in
-            guard let self = self else {
-                return PassthroughSubject<SKSelectionIdentifiableSequence, Never>()
-            }
-            if let subject = self.itemChangedSubject {
-                return subject
-            }
-            let subject = PassthroughSubject<SKSelectionIdentifiableSequence, Never>()
-            self.itemChangedSubject = subject
-            self.observeAll()
-            return subject
-        }.eraseToAnyPublisher()
-    }()
-    
-    private var itemChangedSubject: PassthroughSubject<SKSelectionIdentifiableSequence, Never>?
+    public private(set) lazy var itemChangedPublisher = observer.eraseToAnyPublisher()
+    private let observer = PassthroughSubject<SKSelectionIdentifiableSequence, Never>()
     
     public private(set) var store: [ID: Element] = [:]
     private var cancelables: [ID: AnyCancellable] = [:]
@@ -42,9 +28,31 @@ public class SKSelectionIdentifiableSequence<Element: SKSelectionProtocol, ID: H
                 id: KeyPath<Element, ID>,
                 isUnique: Bool = true) {
         self.isUnique = isUnique
-        items.forEach { element in
-            update(element, by: id)
-        }
+        self.update(items, by: id)
+    }
+    
+}
+
+public extension SKSelectionIdentifiableSequence {
+    
+    func deselect(id: ID) {
+        store[id]?.isSelected = false
+    }
+    
+    func select(id: ID) {
+        store[id]?.isSelected = true
+    }
+    
+}
+
+public extension SKSelectionIdentifiableSequence {
+    
+    var selectedItems: [Element] {
+        store.values.filter(\.isSelected)
+    }
+    
+    var selectedIDs: [ID] {
+        store.filter(\.value.isSelected).map(\.key)
     }
     
 }
@@ -53,7 +61,22 @@ public extension SKSelectionIdentifiableSequence {
     
     func update(_ element: Element, by id: ID) {
         store[id] = element
-        observe(element, by: id)
+        cancelables[id] = element.selectedPublisher
+            .dropFirst()
+            .removeDuplicates()
+            .sink { [weak self] flag in
+                guard let self = self else { return }
+                self.observer.send(self)
+                if flag {
+                    if self.isUnique {
+                        var ids = Set<ID>(self.selectedIDs)
+                        ids.remove(id)
+                        for id in ids {
+                            self.deselect(id: id)
+                        }
+                    }
+                }
+            }
     }
     
     func update(_ element: Element, by keyPath: KeyPath<Element, ID>) {
@@ -65,6 +88,10 @@ public extension SKSelectionIdentifiableSequence {
             update(element, by: keyPath)
         }
     }
+    
+}
+
+public extension SKSelectionIdentifiableSequence {
     
     func removeAll() {
         store.removeAll()
@@ -78,76 +105,6 @@ public extension SKSelectionIdentifiableSequence {
     
     func contains(id: ID) -> Bool {
         return store[id] != nil
-    }
-    
-    func deselect(id: ID) {
-        store[id]?.isSelected = false
-    }
-    
-    func select(id: ID) {
-        store[id]?.isSelected = true
-        maintainUniqueIfNeed(exclude: id)
-    }
-    
-}
-
-private extension SKSelectionIdentifiableSequence {
-    
-    func observeAll() {
-        guard itemChangedSubject != nil else {
-            return
-        }
-        for (id, item) in self.store {
-            self.observe(item, by: id)
-        }
-    }
-    
-    func observe(_ element: Element?, by id: ID) {
-        guard itemChangedSubject != nil else {
-            return
-        }
-        guard let element = element else {
-            cancelables[id] = nil
-            return
-        }
-        observe(element, by: id)
-    }
-    
-    func observe(_ element: Element, by id: ID) {
-        cancelables[id] = element
-            .selectedPublisher
-            .dropFirst()
-            .sink(receiveValue: { [weak self] flag in
-                guard let self = self else { return }
-                if flag {
-                    self.store[id]?.isSelected = true
-                    if !self.maintainUniqueIfNeed(exclude: id) {
-                        self.itemChangedSubject?.send(self)
-                    }
-                } else {
-                    self.deselect(id: id)
-                    self.itemChangedSubject?.send(self)
-                }
-            })
-    }
-    
-    @discardableResult
-    func maintainUniqueIfNeed(exclude id: ID) -> Bool {
-        guard isUnique else {
-            return false
-        }
-        
-        var result = false
-        store
-            .filter({ $0.key != id })
-            .map(\.value)
-            .filter(\.isSelected)
-            .forEach { element in
-                element.isSelected = false
-                result = true
-            }
-        
-        return result
     }
     
 }

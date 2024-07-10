5fbba1b5151a2efe5ae4f838aed3168db145cc78
linhey <is.linhey@outlook.com>
2024-07-10 15:31:05 +0800

    chore: SKCPluginAdjustAttributes 增强

--- Sources/SectionUI/CollectionView/SectionCollectionView/FlowLayout/SKCPluginAdjustAttributes.swift
+++ Sources/SectionUI/CollectionView/SectionCollectionView/FlowLayout/SKCPluginAdjustAttributes.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by linhey on 2024/6/19.
 //
@@ -8,18 +8,29 @@
 import Foundation
 import UIKit
 
+/// A struct that represents adjustments to be applied to layout attributes.
 public struct SKCPluginAdjustAttributes {
-    
+
+    /// A context that holds the plugin and the attributes to be adjusted.
     public struct Context {
         public let plugin: SKCLayoutPlugin
         public var attributes: UICollectionViewLayoutAttributes
     }
     
+    /// A typealias for a style adjustment function.
     public typealias Style = SKInout<Context>
     
+    /// The section to which the adjustments apply.
     public var section: SKBindingKey<Int>?
+    
+    /// The style adjustments to be applied.
     public var style: Style
     
+    /// Initializes a new `SKCPluginAdjustAttributes`.
+    ///
+    /// - Parameters:
+    ///   - section: The section to which the adjustments apply.
+    ///   - style: The style adjustments to be applied.
     public init(section: SKBindingKey<Int>?, _ style: Style) {
         self.style = style
         self.section = section
@@ -29,6 +40,33 @@ public struct SKCPluginAdjustAttributes {
 
 public extension SKInout where Object == SKCPluginAdjustAttributes.Context {
     
+    /// Reverses the header and section inset.
+    static var reverseHeaderAndSectionInset: SKInout<Object> {
+        .set { context in
+            guard context.attributes.representedElementCategory == .supplementaryView,
+                  context.attributes.representedElementKind == UICollectionView.elementKindSectionHeader else {
+                return context
+            }
+            let inset = context.plugin.insetForSection(at: context.attributes.indexPath.section)
+            context.attributes.frame.origin.y += inset.top
+            return context
+        }
+    }
+    
+    /// Reverses the footer and section inset.
+    static var reverseFooterAndSectionInset: SKInout<Object> {
+        .set { context in
+            guard context.attributes.representedElementCategory == .supplementaryView,
+                  context.attributes.representedElementKind == UICollectionView.elementKindSectionHeader else {
+                return context
+            }
+            let inset = context.plugin.insetForSection(at: context.attributes.indexPath.section)
+            context.attributes.frame.origin.y -= inset.bottom
+            return context
+        }
+    }
+    
+    /// Fixes the size of supplementary views.
     static var fixSupplementaryViewSize: SKInout<Object> {
         .set { context in
             guard context.attributes.representedElementCategory == .supplementaryView else {
